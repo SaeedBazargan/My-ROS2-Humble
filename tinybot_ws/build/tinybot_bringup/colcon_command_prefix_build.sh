4c4aa222e698550dc8de803fdda52ad3ba7a7801
SaeedBazargan <bazargan0241private@gmail.com>
2025-02-07 14:24:01 -0500

    bringup and color was added to gazebo

---
+++ tinybot_ws/build/tinybot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sbzrobot/ROS2-Projects/My-ROS2-Humble/tinybot_ws/install/tinybot_description/share/tinybot_description/package.sh"
