5b1aeddfefec000d60b90fc0c19806d51cb1bd2a
SaeedBazargan <bazargan0241private@gmail.com>
2025-02-18 10:42:43 -0500

    its done. Now it can add to the counter and send it with ROS2_msg

---
+++ microROS/microros_ws/build/micro_ros_agent/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sbzrobot/ROS2-Projects/My-ROS2-Humble/microROS/microros_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
