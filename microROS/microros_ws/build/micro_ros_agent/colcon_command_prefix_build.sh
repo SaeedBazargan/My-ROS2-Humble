2ae2e4ca5a4e718c9a001b01bb526c64f462f5fe
Saeed Bazargan <bazargan0241@hotmail.com>
2025-02-28 19:50:08 +0330

    I deleted the codes for the ESP32 and i want to test the STM32 discovery Board.

--- microROS/microros_ws/build/micro_ros_agent/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/sbzrobot/ROS2-Projects/My-ROS2-Humble/microROS/microros_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
--- microROS/microros_ws/build/micro_ros_msgs/micro_ros_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sbzrobot/ROS2-Projects/My-ROS2-Humble/microROS/microros_ws/build/micro_ros_msgs/micro_ros_msgs__py
-# Build directory: /home/sbzrobot/ROS2-Projects/My-ROS2-Humble/microROS/microros_ws/build/micro_ros_msgs/micro_ros_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
